b14a0e10e5b438e67f881bd06a0f0b1ab5c9446b
Felipe Quintero <ffquint@gmail.com>
2021-03-23 21:24:44 -0400

    installed styled components. Working on basic styling for App.js

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
+import styled from 'styled-components'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <mainDiv>
+      <header>
         <p>
           Water My Plants
         </p>
       </header>
-    </div>
+    </mainDiv>
   );
 }
 
 export default App;
+
+const mainDiv = styled.div`
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 6px;
+  box-shadow: "0px 1px 6px -2px rgb(128, 127, 127)";
+  margin: 16px 8px;
+  padding: 16px;
+  background-color: white;
+`;
