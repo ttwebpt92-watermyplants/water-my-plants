a72069e9eefe5f9fa32dd1167517c7366a9f3a03
Felipe Quintero <ffquint@gmail.com>
2021-03-23 21:58:47 -0400

    Wrote styling for divs, h1 and navbuttons. Added framework for main page with placeholder text.

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,26 +1,105 @@
 import React from 'react'
-import styled from 'styled-components'
+import { NavLinks } from 'react-router'
+import styled, { css } from 'styled-components'
 
 import './App.css';
 
+
 function App() {
+
+
   return (
-    <mainDiv>
-      <header>
-        <p>
-          Water My Plants
-        </p>
-      </header>
-    </mainDiv>
+    <MainDiv>
+      <HeaderDiv>
+        Water My Plants
+      </HeaderDiv>
+      <NavBar>
+        <NavButton>Home</NavButton>
+        <NavButton>Register</NavButton>
+        <NavButton>Login</NavButton>
+        <NavButton>Help</NavButton>
+      </NavBar>
+
+      <StyledDiv>
+        <h2>Keep Track Of All Your Beloved Plants!</h2>
+        <p>Lorem ipsum dolorem.</p>
+      </StyledDiv>
+
+      <StyledDiv>
+        <h2>Set A Daily/Weekly Schedule!</h2>
+        <p>Lorem ipsum dolorem.</p>
+      </StyledDiv>
+
+      <StyledDiv>
+        <h2>Search From Our Large Database Of Plants!</h2>
+        <p>Lorem ipsum dolorem.</p>
+      </StyledDiv>
+
+      <StyledDiv>
+        <h2>Easily Edit Your Saved Schedules!</h2>
+        <p>Lorem ipsum dolorem.</p>
+      </StyledDiv>
+
+    </MainDiv>
   );
 }
 
 export default App;
 
-const mainDiv = styled.div`
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
   border: 1px solid rgb(210, 210, 210);
   border-radius: 6px;
-  box-shadow: "0px 1px 6px -2px rgb(128, 127, 127)";
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  margin: 16px 8px;
+  padding: 16px;
+  background-color: white;
+`;
+
+const HeaderDiv = styled.h1`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 6px;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  margin: 16px 8px;
+  padding: 16px;
+  background-color: white;
+`;
+
+const NavBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 6px;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  margin: 16px 8px;
+  padding: 16px;
+  background-color: white;
+`;
+
+const NavButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  border: 1px solid teal;
+  border-radius: 6px;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  margin: 16px 8px;
+  padding: 16px;
+  background-color: white;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 6px;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
   margin: 16px 8px;
   padding: 16px;
   background-color: white;
