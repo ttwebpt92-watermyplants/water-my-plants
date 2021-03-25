e41aa055ae983b822595968dd5304f474d357b6b
Felipe Quintero <ffquint@gmail.com>
2021-03-24 20:06:58 -0400

    created sign up form and started working on form component.

---
+++ water-my-plants/src/SignupForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function SignupForm() {
+
+
+    return (
+        //placeholder code for sign up form
+        <MainDiv>
+            
+        </MainDiv>
+    )
+}
+
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 6px;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  margin: 16px 8px;
+  padding: 16px;
+  background-color: white;
+`;
+
+const FormDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 6px;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  margin: 16px 8px;
+  padding: 16px;
+  background-color: white;
+`;
