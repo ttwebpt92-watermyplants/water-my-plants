19fc05bc1458c51fb9ec02bb357fdd75926c6ff1
milklot <vbiostflex@gmail.com>
2021-03-27 23:17:57 -0400

    add stateful logic in SignUpForm component

--- water-my-plants/src/SignupForm.js
+++ water-my-plants/src/SignupForm.js
@@ -4,10 +4,11 @@ import { useHistory } from 'react-router-dom';
 import styled from 'styled-components'
 
 const initialState = {
-    name:"",
+    username:"",
     password:"",
     phone_number:"",
-    error_msg:""
+    error_msg:"",
+    error_visible: false
 };
 
 export default function SignupForm(props) {
@@ -25,6 +26,39 @@ export default function SignupForm(props) {
     const submitForm = (event) => {
         event.preventDefault();
         console.log("form submitted: ", newUser);
+        let usernameLength = newUser.username.length;
+        let passwordLength = newUser.password.length;
+        let phoneNumberLength = newUser.phone_number.length;
+
+        if (usernameLength < 4) {
+            setNewUser({
+                ...newUser,
+                error_visible:true,
+                error_msg:"username must be at least 4 charachers"
+            });
+        }
+        else if (passwordLength < 6) {
+            setNewUser({
+                ...newUser,
+                error_visible:true,
+                error_msg:"password must be at least 6 characters"
+            });
+        }
+        else if (phoneNumberLength !== 10) {
+            setNewUser({
+                ...newUser,
+                error_visible:true,
+                error_msg:"invalid phone number"
+            });
+        }
+        else {
+            setNewUser({
+                ...newUser,
+                error_visible:false,
+                error_msg: ""
+            });
+            console.log("success: ", newUser);
+        }
     //     axios.post("http://", newUser) <-- need endpoint for post request
     //         .then((res) => {
     //             console.log(res);
@@ -36,7 +70,6 @@ export default function SignupForm(props) {
     }
 
     return (
-        //placeholder code for sign up form
         <MainForm onSubmit={submitForm}>
         <h2>Sign Up and Get Started!</h2>
             <FormDiv>
@@ -49,19 +82,17 @@ export default function SignupForm(props) {
                         placeholder="Example: help_me"
                         value={newUser.username}
                         onChange={changeHandler}
-                        required
                     />
                 </label>&nbsp;&nbsp;
 
                 <label>Phone Number&nbsp;
                     <input
-                        name="phonenumber"
+                        name="phone_number"
                         type="text"
-                        id="phonenumber"
+                        id="phone_number"
                         placeholder="Example:1234567890"
                         value={newUser.phone_number}
                         onChange={changeHandler}
-                        required
                     />
                 </label>&nbsp;&nbsp;
 
@@ -73,14 +104,29 @@ export default function SignupForm(props) {
                         placeholder="Example:lessThen3"
                         value={newUser.password}
                         onChange={changeHandler}
-                        required
                     />
                 </label>
+                {newUser.error_visible ? <ErrorMsgDiv>
+            <ErrorMsgP>{newUser.error_msg}</ErrorMsgP>
+            </ErrorMsgDiv> : null}
             </FormDiv>
             <SubmitButton type='submit'>Submit!</SubmitButton>
         </MainForm>
     )
 }
+
+const ErrorMsgP = styled.p`
+  font-size: 0.9rem;
+  margin: 0 auto;
+  padding: 0% 1%;
+`;
+
+const ErrorMsgDiv = styled.div`
+  background-color: #FEC1C1;
+  padding: 3% 0%;
+  border-radius: 15px;
+  margin:0 auto;
+`;
 
 const MainForm = styled.form`
   display: flex;
