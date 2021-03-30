65ff495bb956e44d6900802d1b03d9525152e97e
Alecsandra Brewer <alecsandrabrewer@yahoo.com>
2021-03-30 18:59:55 -0500

    plantslist styling updated

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -38,9 +38,9 @@ function App() {
             </Route>
           </Switch> */}
 
-        <AddNewPlant/>
-        {/* <PlantsList/>
-        <UpdateUser/> */}
+
+        <PlantsList/>
+        {/* <UpdateUser/> */}
 
         </MainDiv>
       )
